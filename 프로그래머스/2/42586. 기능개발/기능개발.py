6b523d389fa9c56983b53764e90fa9f83e63d186
hsangk <102145595+hsangk@users.noreply.github.com>
2024-01-23 19:16:14 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,3 @@
-
-
-# def solution(progresses, speeds):
-#     answer = []
-#     while progresses:
-#         # 각 작업에 대해 개발 진도를 업데이트
-#         for i in range(len(progresses)):
-#             progresses[i] += speeds[i]
-#         print(progresses)
-#         # 배포 가능한 작업 확인
-#         count = 0
-#         while progresses and progresses[0] >= 100:
-#             progresses.pop(0)
-#             speeds.pop(0)
-#             count += 1
-
-#         # 배포 가능한 작업이 있다면 배포
-#         if count > 0:
-#             answer.append(count)
-
-#     return answer
-
 def solution(progresses, speeds):
 
     answer = []
@@ -41,3 +19,12 @@ def solution(progresses, speeds):
             print("2")
     answer.append(count)
     return answer
+
+
+
+
+
+
+
+
+
